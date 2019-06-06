c9098c59b5e8d8b6ab2d8611f2d730f0f033eea2
lazaro <laziruspoints@gmail.com>
2019-06-06 02:36:33 -0300

    implementação do CRUD

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -14,6 +14,21 @@ module.exports = {
         //criar produtos
         const products = await Product.create(req.body)
         return res.json(products)
+    },
+    async show(req, res){
+        const product = await Product.findById(req.params.id)
+
+        return res.json(product)
+    },
+    async update(req, res){
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, {new: true})
+
+        return res.json(product)
+    },
+    async destroy(req, res){
+        await Product.findByIdAndDelete(req.params.id)
+
+        return res.send("arquivo deletado")
     }
 
 }
